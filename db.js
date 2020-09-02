4f00a1368ac2278734f9ae44a10b756072ca98f8
hamana030 <scv943@naver.com>
2020-09-02 18:19:48 +0900

    finish 3.0

---
+++ db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+
+db.once("open", handleOpen());
+db.on("error");
